59bf9ab50a96f98a31e7acbfa09effab45347f50
mrpromotion <142316185+mdpromotion@users.noreply.github.com>
2026-04-04 20:52:14 +0300

    feat (assembler): add context, event bus, and controller
    Introduce AssemblerContext and IBugEventBus, update assembler and factories to wire up controllers and lifecycle.
    
    Key changes:
    - Add AssemblerContext struct to carry type, position, view and spawn use case.
    - Change AssemblerOutput to include BugController and provide constructor.
    - Update BugAssembler to accept AssemblerContext and IBugEventBus, create BugController, spawn it and enable its agent.
    - Add Eating state to BugFSMFactory states.
    - Implement BugFactory to set lifeDuration per BugType.
    - Add IBugObjectFactory and IBugEventBus interfaces.
    - Update IBugAssembler.CreateBug signature and add Dispose to IBugController.
    
    These changes wire together object creation, FSM, view/controller setup and event propagation for bug spawning/despawning.

--- Assets/Features/Bug/Application/Factories/BugFactory.cs
+++ Assets/Features/Bug/Application/Factories/BugFactory.cs
@@ -1,4 +1,5 @@
 using Bug.Domain;
+using System;
 
 namespace Bug.Application.Factories
 {
@@ -6,8 +7,14 @@ namespace Bug.Application.Factories
     {
         public Domain.Bug CreateBug(BugType type)
         {
-            var bug = new Domain.Bug(type);
-            return null;
+            float lifeDuration = type switch
+            {
+                BugType.Worker => float.MaxValue,
+                BugType.Predator => 100f, // 100 ticks = 10 seconds
+                _ => throw new ArgumentOutOfRangeException(nameof(type), $"Not expected bug type value: {type}")
+            };
+            var bug = new Domain.Bug(type, lifeDuration);
+            return bug;
         }
     }
 }
